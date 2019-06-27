dee8ff2247512503b6f585a97cb06547f17148f1
pajap <paja.proch@seznam.cz>
2019-06-27 18:08:50 +0200

    admin orders

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/orders")
+ * @Route("admin/orders")
  */
 class OrdersController extends AbstractController
 {
--- src/Form/OrdersType.php
+++ src/Form/OrdersType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Country;
 use App\Entity\Orders;
+use App\Entity\Payment;
+use App\Entity\Transport;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,9 +26,34 @@ class OrdersType extends AbstractType
             ->add('zipcode')
             ->add('totalPrice')
             ->add('status')
-            ->add('country')
-            ->add('transport')
-            ->add('payment')
+
+            ->add('country', EntityType::class, [
+                'class' => Country::class,
+                'choice_label' => 'name',
+            ])
+
+            ->add('transport', EntityType::class, [
+                'class' => Transport::class,
+                'choice_label' => 'name',
+
+                'choice_attr' => function(Transport $transport) {
+                    return ['data-transport' => $transport->getPrice()];
+                }
+
+
+            ])
+
+            ->add('payment', EntityType::class, [
+                'class' => Payment::class,
+                'choice_label' => 'name',
+
+                'choice_attr' => function(Payment $payment) {
+                    return ['data-payment' => $payment->getPrice()];
+                }
+
+
+            ])
+
         ;
     }
 
