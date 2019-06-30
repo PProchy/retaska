f5e916254c6b5f1da2d4fdfe568613c80044c25b
pajap <paja.proch@seznam.cz>
2019-06-30 23:56:35 +0200

    funkce vyhledávání funguje

--- src/Controller/ShowProductsController.php
+++ src/Controller/ShowProductsController.php
@@ -38,6 +38,27 @@ class ShowProductsController extends AbstractController
     }
 
 
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(EntityManagerInterface $entityManager, Request $request,CategoryRepository $categoryRepository)
+    {
+
+        $search = $request->query->get('search');
+        $products = $entityManager
+            ->createQuery("SELECT p FROM " . Product::class . " p WHERE p.name LIKE :search")
+            ->setParameter('search', "%$search%")
+            ->getResult();
+
+        return $this->render('show_products/index.html.twig', [
+            'products' => $products,
+            'searchedFor' => $search,
+            'category' => $categoryRepository->findAll()
+
+        ]);
+    }
+
+
 
 
 
